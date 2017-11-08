0b0df137a07a3225cbb32769be329adbfa58ed27
gonzo.bard <gonzo.bard@yandex.ru>
2017-11-08 08:34:33 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * GKislin
@@ -28,8 +28,33 @@ public class UserMealsUtil {
 //        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+    //    Реализовать метод UserMealsUtil.getFilteredWithExceeded:
+    //        -  должны возвращаться только записи между startTime и endTime
+    //        -  поле UserMealWithExceed.exceed должно показывать,
+    //    превышает ли сумма калорий за весь день параметра метода caloriesPerDay
+    //
+    //    Т.е UserMealWithExceed - это запись одной еды, но поле exceeded будет одинаково для всех записей за этот день.
+    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay)
+    {
+        Map<LocalDate, Integer> map = new HashMap<>();
+        List<UserMealWithExceed> res = new ArrayList<>();
+
+        for (UserMeal userMeal : mealList)
+            map.merge(userMeal.getDateTime().toLocalDate(), userMeal.getCalories(), (a, b) -> a + b);
+
+        for (UserMeal userMeal : mealList)
+        {
+            LocalTime localTime = userMeal.getDateTime().toLocalTime();
+            LocalDate localDate = userMeal.getDateTime().toLocalDate();
+
+            if (localTime.isAfter(startTime) && localTime.isBefore(endTime))
+                res.add(new UserMealWithExceed(
+                        userMeal.getDateTime(),
+                        userMeal.getDescription(),
+                        userMeal.getCalories(),
+                        map.getOrDefault(localDate, 0) > caloriesPerDay)
+                );
+        }
+        return res;
     }
 }
